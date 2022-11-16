98451caab5394d9b227a9511a442eebc1ab215a8
bofmar <bofmar@outlook.com>
2022-11-16 17:16:44 +0200

    Finish header

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../images/airbnb-logo.png';
+
+export default function Navbar() {
+  return (
+    <header>
+      <nav>
+        <img src={logo} alt='Airbnb logo' />
+      </nav>
+    </header>
+  )
+}
